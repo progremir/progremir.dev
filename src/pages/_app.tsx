9281e29a64c208b9ccd483cc404869a70413e5ca
Emir Amanbekov <amanbekoff@gmail.com>
2023-03-06 17:57:08 +0300

    feat(analytics): adds analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
